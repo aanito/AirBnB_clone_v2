7993e23f389095552c62267e74152cadb36e1b39
aanito <howmarad@gmail.com>
2023-07-09 17:20:24 +0300

    Edited input text for fake html

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
-#This will set everything up
+# This sets up my web servers for the deployment of web_static
 sudo apt-get -y update
 sudo apt-get-y upgrade
 sudo apt-get install -y nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
+echo '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '43i \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
